5dec4cf16069adc573baa72704e42b08c5fe15d2
1Shibam <hehe.shibam@gmail.com>
2025-03-27 14:30:03 +0530

    update meal provider

--- lib/providers/meal_service_state_provider.dart
+++ lib/providers/meal_service_state_provider.dart
@@ -1,22 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meal_pay/models/meal_model.dart';
 import 'package:meal_pay/services/meal_srvice.dart';
-import 'package:meal_pay/widgets/custom_snackbar.dart';
 
 class MealServiceNotifier extends StateNotifier<AsyncValue<List<MealModel>>> {
   final MealService meals;
-  MealServiceNotifier(this.meals) : super(const AsyncValue.loading());
+  MealServiceNotifier(this.meals) : super(const AsyncValue.loading()) {
+    fetchMealsData();
+  }
 
-  Future<void> fetchMealsData(BuildContext context) async {
+  Future<void> fetchMealsData() async {
     try {
       final data = await meals.loadMealData();
       state = AsyncValue.data(data);
     } catch (error, stackTrace) {
       state = AsyncValue.error(error, stackTrace);
-      if (context.mounted) {
-        customSnackbar(context: context, message: 'Something went wrong!!');
-      }
     }
   }
 }
