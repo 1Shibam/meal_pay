5c0b51cfdd7ed9a65543f946ab9ef71e088698cf
1Shibam <hehe.shibam@gmail.com>
2025-03-27 13:32:43 +0530

    adds meal service state provider

---
+++ lib/providers/meal_service_state_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meal_pay/models/meal_model.dart';
+import 'package:meal_pay/services/meal_srvice.dart';
+import 'package:meal_pay/widgets/custom_snackbar.dart';
+
+class MealServiceNotifier extends StateNotifier<AsyncValue<List<MealModel>>> {
+  final MealService meals;
+  MealServiceNotifier(this.meals) : super(const AsyncValue.loading());
+
+  Future<void> fetchMealsData(BuildContext context) async {
+    try {
+      final data = await meals.loadMealData();
+      state = AsyncValue.data(data);
+    } catch (error, stackTrace) {
+      state = AsyncValue.error(error, stackTrace);
+      if (context.mounted) {
+        customSnackbar(context: context, message: 'Something went wrong!!');
+      }
+    }
+  }
+}
